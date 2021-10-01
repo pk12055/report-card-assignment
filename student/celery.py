83d84fb61b444eb92b3eb0b96214f47a4c6c7095
vishal <vishal.rocks24@gmail.com>
2021-10-01 15:16:15 +0530

    added import

--- student/celery.py
+++ student/celery.py
@@ -17,7 +17,7 @@ app = Celery('student')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
--- student/settings.py
+++ student/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,9 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'rest_framework',
-    'djcelery',
-    'django_celery_results',
-    'djcelery_email',
 ]
 
 MIDDLEWARE = [
